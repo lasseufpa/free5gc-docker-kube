9676b721756eb0902166beb92af179fc31336773
VirgíniaTavares <briosovirginia@gmail.com>
2020-05-06 20:51:18 -0300

    putting ips

--- cluster/amf/setup-lasse.sh
+++ cluster/amf/setup-lasse.sh
@@ -4,6 +4,15 @@ cd support/freeDiameter
 ./make_certs.sh .
 cd ../..
 make install
+
 cp /free5gc/install/etc/free5gc/free5gc-lasseconfiguration.conf /free5gc/install/etc/free5gc/free5gc.conf
 cp /free5gc/install/etc/free5gc/freeDiameter/amf-lasseconfiguration.conf /free5gc/install/etc/free5gc/freeDiameter/amf.conf
+
+#environment variable on the command line in correct order (MONGO, HSS, AMF, UPF, SMF, PCRF)
+sed -i "s|IP_MONGO|$1|g" /free5gc/install/etc/free5gc/free5gc.conf
+sed -i "s|IP_AMF|$3|g"  /free5gc/install/etc/free5gc/free5gc.conf
+sed -i "s|IP_SMF|$5|g"  /free5gc/install/etc/free5gc/free5gc.conf
+sed -i "s|IP_AMF|$3|g"  /free5gc/install/etc/free5gc/freeDiameter/amf.conf
+sed -i "s|IP_HSS|$2|g"  /free5gc/install/etc/free5gc/freeDiameter/amf.conf
+
 ./free5gc-amfd
