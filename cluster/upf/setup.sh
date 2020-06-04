5b7a58f2320cf0dde84bf39ae921f0799bd87f9e
VirgíniaTavares <briosovirginia@gmail.com>
2020-06-03 21:19:25 -0300

    fix ipv6 uptun interface setup

--- cluster/upf/setup.sh
+++ cluster/upf/setup.sh
@@ -14,6 +14,7 @@ ip tuntap add name uptun mode tun
 ip addr add 45.45.0.1/16 dev uptun
 ip link set uptun up
 sh -c "echo 'net.ipv6.conf.uptun.disable_ipv6=0' > /etc/sysctl.d/30-free5gc.conf"
+sysctl -p /etc/sysctl.d/30-free5gc.conf
 ip addr add cafe::1/64 dev uptun
 sh -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
